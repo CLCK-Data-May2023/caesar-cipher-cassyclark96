dbf6acd3251fbdc1dc2d1779e1d83c197847470c
Cassy Clark <cassyclark@cassys-air.attlocal.net>
2023-08-16 19:24:53 -0400

    testing again

--- cipher.py
+++ cipher.py
@@ -1,24 +1,24 @@
 # add your code here
 #create alphabet list
 letters = "abcdefghijklmnopqrstuvwxyz"
+shift = 5
+input_sentence = input("Please enter a sentence: ")
 
 #function to shift letters in original message over 5 spaces
 def encrypt(sentence, shift):
 
-    encrypted_sentence = " "
+    new_sentence = " "
 
     for letter in sentence:
         if letter in letters:
             index = letters.index(letter)
             shifted_index = (index + shift) % len(letters)
-            encrypted_sentence += letters[shifted_index]
+            new_sentence += letters[shifted_index]
         else:
-            encrypted_sentence += letter
+            new_sentence += letter
 
-    return encrypted_sentence
+    return new_sentence
 
-shift = 5
-input_sentence = input("Please enter a sentence:")
 encrypted_sentence = encrypt(input_sentence.lower(), shift)
 
-print("The encrypted sentence is:", encrypted_sentence)
+print("The encrypted sentence is:" + encrypted_sentence)
